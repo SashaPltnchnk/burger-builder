40097327d8533b1f54d9ffbdeb90a741edd13684
ZDubina <ZDubinaQ@gmail.com>
2019-04-02 19:28:04 +0300

    connected to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -78,7 +79,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+       // alert('You continue!');
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer:{
+               name: 'Zi',
+               address: {
+                   street: 'Testeryy 1',
+                   zipCode: '16400',
+                   country: 'Canada'
+               },
+               email: 'test@test.com'
+           },
+           deliveryMethod: 'fastest'
+       }
+        axios.post('/orders.json', order)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
     render () {
