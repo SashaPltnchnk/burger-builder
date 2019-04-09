40e06dd1f74c205a1124136afaa90f98bbf7bbdc
ZDubina <ZDubinaQ@gmail.com>
2019-04-09 17:22:07 +0300

    added an order page + implemented nav links + outputting the orders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,37 +92,16 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
        // alert('You continue!');
-    //    this.setState({ loading: true });
-    //    const order = {
-    //        ingredients: this.state.ingredients,
-    //        price: this.state.totalPrice,
-    //        customer:{
-    //            name: 'Zi',
-    //            address: {
-    //                street: 'Testeryy 1',
-    //                zipCode: '16400',
-    //                country: 'Canada'
-    //            },
-    //            email: 'test@test.com'
-    //        },
-    //        deliveryMethod: 'fastest'
-    //    }
-    //     axios.post('/orders.json', order)
-    //         .then(res => {
-    //             this.setState({ loading: false, purchasing: false });
-    //         })
-    //         .catch(err => {
-    //             this.setState({ loading: false, purchasing: false });
-    //         });
-    const queryParams = [];    
-    for (let i in this.state.ingredients) {
-        queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
-    }
-    const queryString = queryParams.join('&');
-    this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+        const queryParams = [];    
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+                pathname: '/checkout',
+                search: '?' + queryString
+            });
     }
 
     render () {
