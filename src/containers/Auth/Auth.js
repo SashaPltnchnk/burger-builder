6e80fb558a966916c37797ac2726c5fac43ef905
SashaPltnchnk <ZDubinaQ@gmail.com>
2019-07-19 11:22:17 +0300

    implemented redirecting users to diff pages

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -43,6 +43,13 @@ class Auth extends Component {
         isSignUp: true
     }
 
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+    
+
     checkValidity(value, rules) {
         let isValid = true;
 
@@ -132,7 +139,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if(this.props.isAuth) {
-            authRedirect = <Redirect to="/" />
+            authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
@@ -155,13 +162,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuth: state.auth.token !== null
+        isAuth: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
